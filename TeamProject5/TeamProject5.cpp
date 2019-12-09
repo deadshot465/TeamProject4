8a431b770a65a7126fd9e3a7b222fadaf31f9a61
Chehui Chou <deadshot465@users.noreply.github.com>
2019-12-09 15:04:48 +0900

    Initial commit.

---
+++ TeamProject5/TeamProject5.cpp
@@ -0,0 +1,32 @@
+﻿// TeamProject5.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <raylib.h>
+
+int main()
+{
+    InitWindow(1024, 768, "Hello");
+
+    SetTargetFPS(60);
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+
+		ClearBackground(RAYWHITE);
+
+		DrawText("FUCK OFF!!!", 190, 200, 20, LIGHTGRAY);
+		DrawText("FUCK OFF!!!", 380, 300, 30, LIGHTGRAY);
+		DrawText("FUCK OFF!!!", 570, 400, 40, LIGHTGRAY);
+		DrawText("FUCK OFF!!!", 760, 500, 50, LIGHTGRAY);
+
+		RED
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+
+	return 0;
+}
